b7d5034e46803d312b6743cc7325587f1938bbd4
Devid <dmo34@njit.edu>
2026-04-24 20:57:17 -0400

    Added simple username support (not unique yet). Each client that connects is prompted for a username by server, messages show username that sent them/ Also added server callouts that notify other clients when a user has joined or left the chat.

--- ServerMain.java
+++ ServerMain.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.*;
 
 public class ServerMain {
-    public static List<ClientHandler> clients = new ArrayList<>(); //shared list of clients, not using yet
+    public static List<ClientHandler> clients = new ArrayList<>(); //shared list of clients
     public static void main(String[] args) {
         try {
             int port = 1234;
@@ -14,7 +14,7 @@ public class ServerMain {
                 Socket socket = serverSocket.accept();
                 System.out.println("Client has connected: " + socket.getInetAddress()); //get ip of conn client
                 
-                ClientHandler clientThread = new ClientHandler(socket);
+                ClientHandler clientThread = new ClientHandler(socket); //wrap socket in handler
                 clients.add(clientThread);
                 clientThread.start(); //creates new thread for each client
             }
@@ -27,6 +27,7 @@ class ClientHandler extends Thread {
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
+    private String username;
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
@@ -35,30 +36,43 @@ class ClientHandler extends Thread {
         //will handle client comms
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true); //true is for autoflush, makes sure data is sent immediately across network
-            out.println("Connected to server.");
+            out = new PrintWriter(socket.getOutputStream(), true); //autoflush, forces data to be sent right away across network
+            
+            out.println("Enter username: ");
+            username = in.readLine();
+            if (username == null || username.trim().isEmpty()) { //null = if user disconns before entering username or just presses enter, username becomes anonymous
+                username = "Anonymous";
+            }
+
+            out.println("Welcome, " + username + "!");
+            System.out.println(username + " connected from: " + socket.getInetAddress());
+            broadcast("[SERVER]: " + username + " has joined the chat.");
 
             String message;
             //listen for client messages
             while ((message = in.readLine()) != null) { //read input and also check if client still conn
                 System.out.println("[SERVER RECEIVED]: " + message);
 
-                broadcast(message); //broadcast it to all connected clients
+                broadcast(username + ": " + message); //broadcast it to all connected clients
             }
         } catch (IOException e) {
             System.out.println("Client disconnected: " + socket.getInetAddress()); //show ip of disconn client, maybe change later since its not safe to show ip
         } finally {
             try {
                 ServerMain.clients.remove(this); //remove this client from list when it disconns
+                broadcast("[SERVER]: " + username + " has left the chat.");
                 socket.close(); //makes so that socket closes no matter what
             } catch (Exception e) {
                 e.printStackTrace(); //print report about exception
             } 
         }
     }
-    private void broadcast(String message) { //priv because no other class should be allowed to use it
+    private void broadcast(String message) {
         for (ClientHandler clientThread : ServerMain.clients) {
             clientThread.out.println(message);
+            //if (clientThread != this) { //server sends message to every client except sender client
+                //clientThread.out.println(message);
+            //}
         }
     }
 }
